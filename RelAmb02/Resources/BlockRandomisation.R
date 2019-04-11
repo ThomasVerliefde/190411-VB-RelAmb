8bb7fac06f8929e6de1afd177ee46fc397e27bb2
Thomas Verliefde <tverliefde@hotmail.com>
2019-04-11 16:25:32 +0200

    fixed blockrandomisation for current exp

--- RelAmb02/Resources/BlockRandomisation.R
+++ RelAmb02/Resources/BlockRandomisation.R
@@ -1,14 +1,16 @@
 library(magrittr)
 
-combinat::permn(c(0,1,2,3)) %>%
-  unique %T>%
-  {set.seed(20181001)} %>%
+list(0:1) %>%
+  rep(3) %>%
+  expand.grid %>%
+  tidyr::unite(sep="") %>%
+  unlist %>%
+  combinat::permn(.) %T>%
+  {set.seed(20190411)} %>%
   sample(
     size = 500,
     replace = T
   ) %>% unlist %>% paste(collapse = " ") %>%
   write(
     "BlockRandomisation.txt"
-    )
-  
-  
+  )
